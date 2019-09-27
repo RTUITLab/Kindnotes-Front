e60d845aef76c7a718dfc8ebf2048016aeeeaf63
Maksim <maksalmak@gmail.com>
2019-09-27 21:33:12 +0300

    Setup api generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiModule } from './api/api.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiModule.forRoot({ rootUrl: "http://10.70.3.91:5000" })
   ],
   providers: [],
   bootstrap: [AppComponent]
