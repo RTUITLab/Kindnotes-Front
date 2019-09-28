3c9b003ac63884e48bad7f490e4dc4f7a2222ad5
Mikhail <wowivan2011@yandex.ru>
2019-09-28 14:32:30 +0300

    add registration

--- src/app/authentification-page/registration/registration.component.ts
+++ src/app/authentification-page/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from '@angular/forms';
+import { IdentityService } from 'src/app/api/services';
+import { RegisterModel } from 'src/app/api/models';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  regForm;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private identity: IdentityService) {
+    this.regForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      middleName: '',
+      email: '',
+      password: '',
+      repPassword: ''
+    })
+  }
 
   ngOnInit() {
   }
 
+  async onSubmit(regData: RegisterModel) {
+    try {
+      const r = await this.identity.apiIdentityRegisterPost$Json({body: regData}).toPromise();
+    } catch (ex) {
+      alert(`Чет не так! ${ex}`);
+    }
+  }
 }
